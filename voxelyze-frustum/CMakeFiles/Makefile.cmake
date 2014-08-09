713336f4020355cc9461aaa0d484a8c5cdd67e3d
Ubuntu <ubuntu@ip-10-220-5-184.us-west-2.compute.internal>
2014-08-09 15:47:09 +0000

    reworking

--- voxelyze-frustum/CMakeFiles/Makefile.cmake
+++ voxelyze-frustum/CMakeFiles/Makefile.cmake
@@ -51,5 +51,5 @@ SET(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 SET(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/voxelyze.dir/DependInfo.cmake"
+  "CMakeFiles/voxelize.dir/DependInfo.cmake"
   )
---
+++ voxelyze-frustum/CMakeFiles/voxelize.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/voxelize.dir/Voxelyze/main.cpp.o"
+  "voxelize.pdb"
+  "voxelize"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/voxelize.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
